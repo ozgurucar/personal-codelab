0218e6837fb27f0b388d0e0b1b4ab5e12b3c5f5c
Özgür Uçar <ucar.ozgur@outlook.com>
2025-07-22 23:43:06 +0300

    responsive-navbar 50% completed

---
+++ web-dev/CSSFCC/responsive-navbar/script.js
@@ -0,0 +1,11 @@
+const navbar = document.getElementById('navbar');
+
+function openSidebar() {
+navbar.classList.add('show');
+
+}
+
+
+function closeSidebar() {
+navbar.classList.remove('show');
+}
