3682e7d62f04d0f494977b5c9fb65c5d8fa5e9c2
ozgurucar <ucar.ozgur@outlook.com>
2025-08-10 12:17:34 +0200

    csharp project initialization

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,5 @@
+﻿String name = "Bob";
+int messageCount = 3;
+double temperature = 34.4;
+
+Console.WriteLine($"Hello, {name}! You have {messageCount} messages in your inbox. The temperature is {temperature} celcius");
